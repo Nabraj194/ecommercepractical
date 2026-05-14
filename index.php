c9ccdc8afa11920cf3168b1ee0916979d2c6be22
Nabraj Regmi <regminabaraj194@gmail.com>
2026-05-14 20:23:38 +0545

    its ok

--- index.php
+++ index.php
@@ -1,137 +1,546 @@
 <?php
 include 'php/config.php';
 
-$product_query = mysqli_query($conn, "SELECT * FROM products");
-    $selected_category = "";
+session_start();
 
-    if(isset($_GET['category'])){
-       $selected_category = $_GET['category'];
+$selected_category = "";
+
+if(isset($_GET['category'])){
+    $selected_category = $_GET['category'];
 }
 
-    $count_query = mysqli_query($conn,"SELECT SUM(quantity) as total FROM cart 
-    WHERE session_id='".session_id()."'");
+$search = trim($_GET['search'] ?? '');
 
-    $count = mysqli_fetch_assoc($count_query);
+/* CART COUNT */
+$count_query = mysqli_query($conn,"
+    SELECT SUM(quantity) as total 
+    FROM cart 
+    WHERE session_id='".session_id()."'
+");
+
+$count = mysqli_fetch_assoc($count_query);
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Handmade Haven</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ArtNest</title>
+
     <link rel="stylesheet" href="css/style.css">
+
+    <style>
+
+        *{
+            margin:0;
+            padding:0;
+            box-sizing:border-box;
+            font-family:Arial, sans-serif;
+        }
+
+        body{
+            background:#f8f5ef;
+        }
+
+        /* NAVBAR */
+
+        .navbar{
+            position:fixed;
+            top:0;
+            left:0;
+            width:100%;
+            height:85px;
+
+            display:flex;
+            justify-content:space-between;
+            align-items:center;
+
+            padding:0 60px;
+
+            background:rgba(70,50,40,0.95);
+
+            z-index:1000;
+        }
+
+        .logo{
+            display:flex;
+            align-items:center;
+            gap:15px;
+        }
+
+        .logo img{
+            width:60px;
+            height:60px;
+            border-radius:50%;
+            object-fit:cover;
+            border:3px solid #d9a441;
+        }
+
+        .logo h2{
+            color:white;
+            font-size:40px;
+        }
+
+        /* NAV MENU */
+
+.nav-menu{
+
+    display:flex;
+
+    align-items:center;
+
+    gap:35px;
+
+    list-style:none;
+}
+
+.nav-menu li{
+    position:relative;
+}
+
+.nav-menu li a{
+
+    color:white;
+
+    text-decoration:none;
+
+    font-size:20px;
+
+    padding:10px;
+
+    display:block;
+}
+
+/* DROPDOWN */
+
+.dropdown-menu{
+
+    display:none;
+
+    position:absolute;
+
+    top:45px;
+
+    left:0;
+
+    min-width:220px;
+
+    background:white;
+
+    border-radius:10px;
+
+    box-shadow:0 5px 15px rgba(0,0,0,0.15);
+
+    z-index:9999;
+
+    list-style:none;
+
+    padding:0;
+
+    margin:0;
+
+    flex-direction:column;
+}
+
+.dropdown-menu li a{
+
+    color:#333 !important;
+
+    padding:14px 18px;
+
+    display:block;
+
+    font-size:16px;
+
+    transition:0.3s;
+    
+}
+
+.dropdown-menu li a:hover{
+
+    background:#f3f3f3;
+
+    color:#d97b49 !important;
+}
+
+/* IMPORTANT */
+
+
+        .cart-icon{
+            font-size:20px;
+        }
+
+        /* HERO */
+
+        .hero{
+            height:100vh;
+
+            background:
+            linear-gradient(rgba(255,255,255,0.4),rgba(255,255,255,0.4)),
+            url('hero.jpg');
+
+            background-size:cover;
+            background-position:center;
+
+            display:flex;
+            justify-content:center;
+            align-items:center;
+
+            text-align:center;
+
+            padding-top:80px;
+        }
+
+        .hero-overlay h1{
+            font-size:75px;
+            color:#3c2415;
+            margin-bottom:20px;
+        }
+
+        .hero-overlay p{
+            font-size:30px;
+            color:#3c2415;
+            margin-bottom:40px;
+        }
+
+        /* SEARCH BAR */
+
+        .search-box{
+            display:flex;
+            justify-content:center;
+        }
+
+        .search-box form{
+            width:700px;
+
+            background:rgba(255,255,255,0.25);
+
+            padding:10px;
+
+            border-radius:20px;
+
+            display:flex;
+            align-items:center;
+
+            backdrop-filter:blur(5px);
+        }
+
+        .search-box input{
+            flex:1;
+
+            padding:18px;
+
+            border:none;
+            outline:none;
+
+            border-radius:12px;
+
+            font-size:18px;
+
+            background:white;
+            color:black;
+        }
+
+        .search-box input::placeholder{
+            color:#777;
+        }
+
+        .search-box button{
+            margin-left:10px;
+
+            padding:18px 35px;
+
+            border:none;
+
+            border-radius:12px;
+
+            background:#d97b49;
+
+            color:white;
+
+            font-size:18px;
+
+            cursor:pointer;
+
+            transition:0.3s;
+        }
+
+        .search-box button:hover{
+            background:#b85f2f;
+        }
+
+        /* CATEGORY */
+
+        .categories{
+            padding:80px 60px;
+            background:white;
+        }
+
+        .categories h2{
+            text-align:center;
+            font-size:45px;
+            margin-bottom:40px;
+            color:#3c2415;
+        }
+
+        .cat-container{
+            display:grid;
+            grid-template-columns:repeat(4,1fr);
+            gap:30px;
+        }
+
+        .cat-box{
+            background:white;
+            border-radius:20px;
+            overflow:hidden;
+
+            box-shadow:0 5px 15px rgba(0,0,0,0.1);
+
+            transition:0.3s;
+        }
+
+        .cat-box:hover{
+            transform:translateY(-5px);
+        }
+
+        .cat-box img{
+            width:100%;
+            height:250px;
+            object-fit:cover;
+        }
+
+        .cat-box h3{
+            text-align:center;
+            padding:20px;
+            color:#3c2415;
+            font-size:28px;
+        }
+
+        .cat-container a{
+            text-decoration:none;
+        }
+
+        /* PRODUCTS */
+
+        .products{
+            padding:80px 60px;
+        }
+
+        .products h2{
+            text-align:center;
+            font-size:45px;
+            margin-bottom:40px;
+            color:#3c2415;
+        }
+
+        .product-grid{
+            display:grid;
+            grid-template-columns:repeat(4,1fr);
+            gap:30px;
+        }
+
+        .product-card{
+            background:white;
+            border-radius:20px;
+            overflow:hidden;
+
+            box-shadow:0 5px 15px rgba(0,0,0,0.1);
+
+            transition:0.3s;
+        }
+
+        .product-card:hover{
+            transform:translateY(-5px);
+        }
+
+        .product-card img{
+            width:100%;
+            height:250px;
+            object-fit:cover;
+        }
+
+        .product-card h4{
+            padding:15px;
+            color:#3c2415;
+            font-size:22px;
+        }
+
+        .product-card p{
+            padding:0 15px 20px;
+            color:#d97b49;
+            font-size:20px;
+            font-weight:bold;
+        }
+
+        .product-card a{
+            text-decoration:none;
+        }
+
+        /* FOOTER */
+
+    </style>
 </head>
+
 <body>
 
-<!-- 🔥 ADD LOADING SCREEN HERE (TOP OF PAGE CONTENT) -->
-<div id="loading-screen">
-    <div class="loader"></div>
-    <h2>Searching Products...</h2>
-</div>
+<!-- NAVBAR -->
 
-<!-- PREMIUM NAVBAR -->
 <nav class="navbar">
+
     <div class="logo">
         <img src="whh.jpeg">
         <h2>ArtNest</h2>
     </div>
 
-<ul class="nav-menu">
+    <ul class="nav-menu">
+
     <li><a href="#">Home</a></li>
 
-  <li class="dropdown">
-    <a href="#category-section">Category</a>
+    <li class="dropdown">
 
-    <ul class="dropdown-menu">
-        <li><a href="category.php?category=handmade">Handmade</a></li>
-        <li><a href="category.php?category=paintings">Paintings</a></li>
-        <li><a href="category.php?category=ceramics">Ceramics</a></li>
-        <li><a href="category.php?category=decor">Decor</a></li>
-    </ul>
-</li>
- <li><a href="php/about.php">About</a></li>
-<li><a href="php/contact.php">Contact</a></li>
-    
+        <a href="#">Category ▾</a>
+
+        <ul class="dropdown-menu">
+
+            <li>
+                <a href="category.php?category=handmade">
+                    Handmade
+                </a>
+            </li>
+
+            <li>
+                <a href="category.php?category=paintings">
+                    Paintings
+                </a>
+            </li>
+
+            <li>
+                <a href="category.php?category=ceramics">
+                    Ceramics
+                </a>
+            </li>
+
+            <li>
+                <a href="category.php?category=decor">
+                    Decor
+                </a>
+            </li>
+
+        </ul>
+
+    </li>
+
+    <li><a href="php/about.php">About</a></li>
+
+    <li><a href="php/contact.php">Contact</a></li>
+
 </ul>
 
     <div class="nav-btns">
-    <a href="login.html">Login</a>
-    <a href="register.html">Register</a>
+        <a href="login.html">Login</a>
+        <a href="register.html">Register</a>
+
         <a href="cart.php" class="cart-icon">
             🛒 <?php echo $count['total'] ?? 0; ?>
         </a>
     </div>
+
 </nav>
 
-<?php
-$search = trim($_GET['search'] ?? '');
-?>
+<?php if($search != '') { ?>
 
-<?php if ($search != '') { ?>
+<section class="products">
 
-    <!-- ================= SEARCH PAGE ================= -->
+    <h2>
+        Search Results for "<?php echo htmlspecialchars($search); ?>"
+    </h2>
 
     <?php
+
     $search_safe = mysqli_real_escape_string($conn, $search);
 
-    $product_query = mysqli_query($conn, "
+    $product_query = mysqli_query($conn,"
         SELECT * FROM products
         WHERE name LIKE '%$search_safe%'
-           OR category LIKE '%$search_safe%'
-           OR description LIKE '%$search_safe%'
+        OR category LIKE '%$search_safe%'
+        OR description LIKE '%$search_safe%'
     ");
+
     ?>
 
-    <section class="products">
-        <h2>Search Results for "<?php echo htmlspecialchars($search); ?>"</h2>
+    <div class="product-grid">
 
-        <div class="product-grid">
-            <?php if (mysqli_num_rows($product_query) > 0) { ?>
-                <?php while ($p = mysqli_fetch_assoc($product_query)) { ?>
-                    <div class="product-card">
-                        <a href="product.php?id=<?php echo $p['id']; ?>">
-                            <img src="images/<?php echo $p['image']; ?>">
-                            <h4><?php echo $p['name']; ?></h4>
-                            <p>Rs. <?php echo $p['price']; ?></p>
-                        </a>
-                    </div>
-                <?php } ?>
-            <?php } else { ?>
-                <p style="text-align:center; font-size:20px; color:red;">
-                    No products found.
-                </p>
+        <?php if(mysqli_num_rows($product_query) > 0){ ?>
+
+            <?php while($p = mysqli_fetch_assoc($product_query)){ ?>
+
+                <div class="product-card">
+
+                    <a href="product.php?id=<?php echo $p['id']; ?>">
+
+                        <img src="images/<?php echo $p['image']; ?>">
+
+                        <h4><?php echo $p['name']; ?></h4>
+
+                        <p>Rs. <?php echo $p['price']; ?></p>
+
+                    </a>
+
+                </div>
+
             <?php } ?>
-        </div>
 
-        <div style="text-align:center; margin-top:30px;">
-            <a href="index.php"
-               style="padding:10px 20px; background:black; color:white; border-radius:10px; text-decoration:none;">
-               Back to Home
-            </a>
-        </div>
-    </section>
+        <?php } else { ?>
+
+            <p>No products found.</p>
+
+        <?php } ?>
+
+    </div>
+
+</section>
 
 <?php } else { ?>
 
-    <!-- ================= HOME PAGE ================= -->
+<!-- HERO -->
 
-    <!-- HERO SECTION -->
-    <section class="hero">
-        <div class="hero-overlay">
-            <h1>Discover Handmade Beauty</h1>
-            <p>Unique Craftsmanship, Beautiful Art, Elegant Decor</p>
+<section class="hero">
 
-            <div class="search-box">
-                <form method="GET" action="index.php" style="display:flex; width:100%;">
-                    <input type="text" name="search" placeholder="Search products...">
-                    <button type="submit">Search</button>
-                </form>
-            </div>
+    <div class="hero-overlay">
+
+        <h1>Discover Handmade Beauty</h1>
+
+        <p>
+            Unique Craftsmanship, Beautiful Art, Elegant Decor
+        </p>
+
+        <div class="search-box">
+
+            <form method="GET" action="index.php">
+
+                <input
+                    type="text"
+                    name="search"
+                    placeholder="Search products..."
+                >
+
+                <button type="submit">
+                    Search
+                </button>
+
+            </form>
+
         </div>
-    </section>
 
-    <!-- CATEGORIES -->
-    <section class="categories" id="category-section">
-        <!-- Y<!-- CATEGORIES -->
+    </div>
+
+</section>
+
+<!-- CATEGORY -->
+
 <section class="categories" id="category-section">
+
     <h2>Explore Categories</h2>
 
     <div class="cat-container">
@@ -165,9 +574,13 @@ $search = trim($_GET['search'] ?? '');
         </a>
 
     </div>
-    </section>
 
-    <!-- TRENDING PRODUCTS -->
+</section>
+
+<?php } ?>
+
+<!-- FOOTER -->
+ <!-- TRENDING PRODUCTS -->
     <section class="products">
         <h2>Trending Products</h2>
 
@@ -192,7 +605,6 @@ $search = trim($_GET['search'] ?? '');
         </div>
     </section>
 
-<?php } ?>
 
 <!-- FOOTER -->
 <footer class="footer">
--- my_database/ecommerce_store.sql
+++ my_database/ecommerce_store.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 14, 2026 at 04:57 AM
+-- Generation Time: May 14, 2026 at 01:00 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -26,56 +26,285 @@ SET time_zone = "+00:00";
 --
 -- Table structure for table `cart`
 --
--- Error reading structure for table ecommerce_store.cart: #1932 - Table &#039;ecommerce_store.cart&#039; doesn&#039;t exist in engine
--- Error reading data for table ecommerce_store.cart: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ecommerce_store`.`cart`&#039; at line 1
+
+CREATE TABLE `cart` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `quantity` int(11) DEFAULT 1,
+  `added_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `session_id` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `contact_messages`
 --
--- Error reading structure for table ecommerce_store.contact_messages: #1932 - Table &#039;ecommerce_store.contact_messages&#039; doesn&#039;t exist in engine
--- Error reading data for table ecommerce_store.contact_messages: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ecommerce_store`.`contact_messages`&#039; at line 1
+
+CREATE TABLE `contact_messages` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `subject` varchar(255) DEFAULT NULL,
+  `message` text DEFAULT NULL,
+  `sent_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `contact_messages`
+--
+
+INSERT INTO `contact_messages` (`id`, `name`, `email`, `subject`, `message`, `sent_at`) VALUES
+(1, 'Nabraj', 'regminabaraj194@gmail.com', NULL, 'your sacebook', '2026-05-14 07:45:19');
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `orders`
 --
--- Error reading structure for table ecommerce_store.orders: #1932 - Table &#039;ecommerce_store.orders&#039; doesn&#039;t exist in engine
--- Error reading data for table ecommerce_store.orders: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ecommerce_store`.`orders`&#039; at line 1
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `total_amount` decimal(10,2) DEFAULT NULL,
+  `order_status` enum('pending','processing','shipped','delivered','cancelled') DEFAULT 'pending',
+  `payment_method` varchar(50) DEFAULT NULL,
+  `order_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `fullname` varchar(255) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `address` text DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `total_price` decimal(10,2) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `user_id`, `total_amount`, `order_status`, `payment_method`, `order_date`, `fullname`, `phone`, `address`, `email`, `total_price`, `created_at`) VALUES
+(1, NULL, NULL, 'pending', NULL, '2026-05-14 07:19:36', 'Nabaraj Regmi', '8876887', 'npj', 'regminabaraj194@gmail.com', 0.00, '2026-05-14 10:22:48'),
+(2, NULL, NULL, 'pending', NULL, '2026-05-14 07:34:53', 'Nabraj Regmi', '999', 'Salyan,Kalimati', 'regminabaraj194@gmail.com', 850.00, '2026-05-14 10:22:48');
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `products`
 --
--- Error reading structure for table ecommerce_store.products: #1932 - Table &#039;ecommerce_store.products&#039; doesn&#039;t exist in engine
--- Error reading data for table ecommerce_store.products: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ecommerce_store`.`products`&#039; at line 1
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `seller_id` int(11) DEFAULT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `stock` int(11) DEFAULT 0,
+  `category` varchar(100) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `sold_count` int(11) DEFAULT 0,
+  `name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `seller_id`, `product_name`, `description`, `price`, `stock`, `category`, `image`, `created_at`, `sold_count`, `name`) VALUES
+(1, 1, '', 'This is rear product', 500.00, 3, 'paintings', 'acc.jpg', '2026-05-14 07:14:45', 0, 'glass');
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `reports`
 --
--- Error reading structure for table ecommerce_store.reports: #1932 - Table &#039;ecommerce_store.reports&#039; doesn&#039;t exist in engine
--- Error reading data for table ecommerce_store.reports: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ecommerce_store`.`reports`&#039; at line 1
+
+CREATE TABLE `reports` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `report_type` varchar(100) DEFAULT NULL,
+  `report_message` text DEFAULT NULL,
+  `report_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `product_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `reviews`
 --
--- Error reading structure for table ecommerce_store.reviews: #1932 - Table &#039;ecommerce_store.reviews&#039; doesn&#039;t exist in engine
--- Error reading data for table ecommerce_store.reviews: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ecommerce_store`.`reviews`&#039; at line 1
+
+CREATE TABLE `reviews` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `rating` int(11) DEFAULT NULL CHECK (`rating` between 1 and 5),
+  `comment` text DEFAULT NULL,
+  `review_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
 --
--- Error reading structure for table ecommerce_store.users: #1932 - Table &#039;ecommerce_store.users&#039; doesn&#039;t exist in engine
--- Error reading data for table ecommerce_store.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ecommerce_store`.`users`&#039; at line 1
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('buyer','seller','admin') DEFAULT 'buyer',
+  `handmade_type` varchar(100) DEFAULT NULL,
+  `bio` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`, `handmade_type`, `bio`, `created_at`) VALUES
+(1, 'aaaaa', 'admin@aaaaa', '$2y$10$fwi1HBZYV7uY6MIvUOBn5usj3o8lEWjvtnbPdS0Tynr8vKJZK1TnO', 'seller', NULL, NULL, '2026-05-14 07:11:39'),
+(2, 'Nabraj', 'root@sss', '$2y$10$PbEhRH6coLL.t.7K1Y2Xne34fV4CdVCjmgqreWiQyyA7We8WKcsEK', 'seller', NULL, NULL, '2026-05-14 07:21:35'),
+(4, 'admin', 'admin@gmail.com', 'admin123', 'admin', NULL, NULL, '2026-05-14 07:33:31');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `contact_messages`
+--
+ALTER TABLE `contact_messages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `seller_id` (`seller_id`);
+
+--
+-- Indexes for table `reports`
+--
+ALTER TABLE `reports`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `contact_messages`
+--
+ALTER TABLE `contact_messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `reports`
+--
+ALTER TABLE `reports`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `reviews`
+--
+ALTER TABLE `reviews`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`seller_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `reports`
+--
+ALTER TABLE `reports`
+  ADD CONSTRAINT `reports_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD CONSTRAINT `reviews_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `reviews_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
